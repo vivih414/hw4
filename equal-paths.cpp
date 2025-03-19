89a3f84abed8647c8efe97c0a7b7cfea160835fc
Viviana Herrera <vh89445@usc.edu>
2025-03-19 23:41:35 +0000

    Completed fully

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,33 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool equalCheck(Node* root, int depth, int& leaf)
+{
+	if(root == nullptr)
+	{
+		return true;
+	}
+	if(root->left == nullptr && root->right == nullptr)
+	{
+		if(leaf == -1)
+		{
+			leaf = depth;
+			return true;
+		}
+		return depth == leaf;
+	}
 
+	bool leftSide = equalCheck(root->left, depth+1, leaf);
+	bool rightSide = equalCheck(root->right, depth+1, leaf);
+
+	return leftSide && rightSide;
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+		int leaf = -1;
 
+		return equalCheck(root, 0, leaf);
 }
 
